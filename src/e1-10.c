e3c4f32c2947c8d5e9582aba103b6fb2e3da4c1a
Jesus Curiel <curieldev@gmail.com>
2023-11-08 18:16:48 -0600

    feat: add exercise 1-10, replace whitespace with visual symbols

---
+++ src/e1-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void) {
+    char c;
+
+    while((c = getchar()) != EOF){
+        if (c == ' ')
+            printf("\\b");
+        else if (c == '\t')
+            printf("\\t");
+        else if (c == '\n')
+            printf("\\n\n");
+        else
+            putchar(c);
+    }
+
+    return 0;
+}
