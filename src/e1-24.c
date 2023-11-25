109b1855d662f06ae8a48c7e3d275ae88fdea96c
Alberto Curiel <curieldev@gmail.com>
2023-11-24 20:48:54 -0600

    feat(e1-24): add stack, and default state handle

--- src/e1-24.c
+++ src/e1-24.c
@@ -1,14 +1,14 @@
+// NOTE: Decided not to use structs as they are not presented yet in the book.
 #include <stdio.h>
 #include <stdbool.h>
 
 #define STACK_SIZE  100
 
 #define DEFAULT     0
-#define INLINE      1   // Inside inline comment
-#define BLOCK       2   // Inside block comment
-#define QUOTE       3   // Inside quotes
+#define COMMENT     1   // Inside block comment
+#define QUOTE       2   // Inside quotes
 
-// Notes
+// Dev Notes
 // Parentheses, brackets and braces can be handled the same.
 // Single and double quotes can be handled the same.
 // No syntax error comes to mind for inline comments.
@@ -19,28 +19,54 @@
 
 int main(void) {
     char prev_c, c;
-    char stack[STACK_SIZE];
+    char syntax_stack[STACK_SIZE];
+    int  line_stack[STACK_SIZE];
     int  stack_idx;
+    int  state;
+    int  line_count;
 
+    state = DEFAULT;
     stack_idx = 0;
     prev_c = '\0';
+    line_count = 1;
     while ((c = getchar()) != EOF) {
-        bool is_inline = (prev_c == '/' && c == '/');
-        bool is_block  = (prev_c == '/' && c == '*');
-        bool is_quote  = (c == '\"' || c == '\'')
-        bool is_pair   = (c == '{' || c == '(' || c == '[');
-
-        // TODO: Reuse previous logic
-        // Symmetric paired symbols share the default state
-
-        if (is_inline || is_block || is_quote || is_pair) {
-            stack[stack_idx++] = c;
+        if (c == '\n') {
+            line_count++;
         }
+
+        
+        if (state == DEFAULT) {
+            if (prev_c == '/' && c == '*')
+                state = COMMENT;
+            else if (c == '\"' || c == '\'')
+                state = QUOTE;
+
+            bool is_pair = (c == '{' || c == '(' || c == '[');
+            if (state != DEFAULT || is_pair) {
+                syntax_stack[stack_idx] = c;
+                line_stack[stack_idx] = line_count;
+                stack_idx++;
+            }
+
+            // TODO: Implement pair exit handling
+            // TODO: Add back inline comment case
+        }
+        else if (state == COMMENT) {
+
+        }
+        else if (state == QUOTE) {
+
+        }
+        else {
+
+        }
+        state = DEFAULT;
 
         prev_c = c;
     }
-    putchar(c); // Print last character
 
+    for (int i = 0; i < stack_idx; i++)
+        printf("%c at line %d\n", syntax_stack[i], line_stack[i]);
 
     return 0;
 }
