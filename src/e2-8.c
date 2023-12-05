7a9c62cafd303a35946a75c94d764c288daaa16e
Alberto Curiel <curieldev@gmail.com>
2023-12-05 08:07:47 -0600

    feat(e2-8): add implementation

--- src/e2-8.c
+++ src/e2-8.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <limits.h>
 
 static unsigned int rightrot(unsigned int x, int n) {
-    return x;
+    size_t bits = sizeof(int) / sizeof(char) * CHAR_BIT;
+    unsigned int result = x;
+
+    if (n > 0 && n < bits) {
+        unsigned int n_bits_mask         = ~(UINT_MAX << n);
+        unsigned int n_bits              = x & n_bits_mask;
+        unsigned int n_bits_rotated_mask = n_bits_mask << (bits - n);
+        unsigned int n_bits_rotated      = n_bits << (bits - n);
+
+        result = (x >> n & ~n_bits_rotated_mask) | n_bits_rotated;
+    }
+    return result;
 }
 
 int main(void) {
-    unsigned int input = 0xFFFFFFFA;
-    int          bits  = 4;
+    unsigned int input = 0x1A2B3C4D;
+    int          bits  = 12;
     printf("Original number: 0x%X\n", input);
     printf("Number rotated %d bits: 0x%X\n", bits, rightrot(input, bits));
 
